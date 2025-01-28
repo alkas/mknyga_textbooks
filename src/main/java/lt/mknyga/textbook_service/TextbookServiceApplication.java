db7e1ee418f1f90f5676689aef286315595d2c26
alkas <erikas.stanys@gmail.com>
2025-01-28 22:44:14 +0200

    First commit

---
+++ src/main/java/lt/mknyga/textbook_service/TextbookServiceApplication.java
@@ -0,0 +1,13 @@
+package lt.mknyga.textbook_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextbookServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextbookServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lt/mknyga/textbook_service/TextbookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.mknyga.textbook_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextbookServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
